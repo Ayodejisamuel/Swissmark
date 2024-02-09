1150669df22187bdeb74988ce8e8933b9e2be7c9
Ayodeji <ayodejisamuel80@gmail.com>
2024-02-09 16:34:24 +0100

    new changes

--- src/component/Banner/Banner.js
+++ src/component/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import hambugger from '../../images/hambugger.png'
+import hambugger1 from '../../images/cancelbugger.png'
 import logo from '../../images/logo-mobile.png'
 import './Banner.css';
 
@@ -8,34 +9,41 @@ import './Banner.css';
 const Banner = () => {
 
   const [navOpen, setNavOpen] = useState(false)
- 
-  const toggleNav  = ( ) => {
+
+  const toggleNav = () => {
     setNavOpen(!navOpen)
   }
   return (
 
     <div id='Banner-container'>
 
- <img src={logo} alt={logo} className='mobile'></img>
+      <img src={logo} alt={logo} className='mobile'></img>
 
-      <div  className='toggle' onClick={toggleNav} >
-        <img src={hambugger} alt={hambugger}   ></img>
+      <div className='toggle' onClick={toggleNav}>
+
+        {navOpen ? <img src={hambugger1} height={30} width={30} alt={hambugger}   ></img>
+
+          :
+
+          <img src={hambugger} alt={hambugger}   ></img>
+        }
+
       </div>
 
 
       <div className={`navbar ${navOpen ? 'visible' : ""}`}>
 
-        <a href='#company'>Company</a>
+        <a className='' href='#company'>Company</a>
         <a href='#who-we-are'>Who we are </a>
         <a href='#what-we-do'>What we do</a>
         <a href='#get-in-touch'>Get in touch</a>
         <a href='#shop'>Shop</a>
-        <a href=''>Solutions</a>
+        <a href='#shop'>Solutions</a>
         <a href='#client'>Our Clientele</a>
         <a href='#blog'>Blog</a>
 
       </div>
-      
+
       <div className='description'>
         Your business and <span>home</span><br />
         <span>can be</span> effortlessly<br />
